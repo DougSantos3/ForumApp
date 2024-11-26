c17adbf374f45367d9bd28765dc4394eee251394
douglas <douglasmartinssantos7@gmail.com>
2024-11-26 16:39:21 -0300

    new libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,8 +61,21 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.recyclerview)
+    // Fragment Testing
+    implementation("androidx.fragment:fragment-testing:1.5.7")
+    // Espresso para testes de UI
+    implementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.test.ext:junit:1.1.5")
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation(libs.androidx.junit.ktx)
+    // Testes unitários
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.mockito:mockito-core:4.11.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+    // Testes instrumentados
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
